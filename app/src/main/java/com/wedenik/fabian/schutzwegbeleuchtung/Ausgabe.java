858c6fb807dbb0a85b9b5a44cf7b00cdb9ec79b4
Fabian Wedenik <fabian@wedenik.com>
2016-09-13 16:08:42 +0200

    Added second screen.

---
+++ app/src/main/java/com/wedenik/fabian/schutzwegbeleuchtung/Ausgabe.java
@@ -0,0 +1,13 @@
+package com.wedenik.fabian.schutzwegbeleuchtung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ausgabe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ausgabe);
+    }
+}
